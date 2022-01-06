45e67885a885e06abdac771016ea7cce4898b86c
JC Carrion <jcarrion@realtysapient.com>
2022-01-06 15:18:58 -0500

    first

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+struct Rectangle {
+    height: u32,
+    width: u32,
+
+}
+
+fn main() {
+    let rect = Rectangle {
+        height:50,
+        width:30,
+    };
+    println!("The area of the rectangle is {}",area(&rect));
+}
+
+fn area(rect:&Rectangle) -> u32 {
+    rect.height * rect.width
+}
